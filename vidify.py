2fbf0f145591a60653fb33e5b4f9bd6420f6e783
Akshay Chalana <ac2zoom@yahoo.com>
2019-07-13 14:23:44 -0700

    Submoduled!

--- vidify.py
+++ vidify.py
@@ -1,5 +1,5 @@
 import vidtext
 
-li = "Text"  # Text Content for video
+li = "Video Rocks"  # Text Content for video
 content = vidtext.summary(li)  # Return List
 vidtext.TextToVideo(content, vs=0.1)  # vs to control the speed of video
