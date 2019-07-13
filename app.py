9203ae49690fb41fcaedf2bc67515b5e03c447f7
Kostya Yatsuk <yatsuk@uw.edu>
2019-07-13 16:48:43 -0700

    Add text overlay onto images

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from google_images_download import google_images_download
 from PIL import ImageFont, Image, ImageDraw
+import cv2
 import os
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ def hello_world():
     
     return render_template('index.html')
 
+
 """
 Loops through all the given keywords and generates slides by 
 finding images for the keywords and overlaying their descriptions onto the images. 
@@ -20,6 +22,13 @@ Saves the resulting images in the 'slides' folder
 @return:            Saves resulting slides to 'slides' folder
 """
 def make_slides(description_map):
+    image_arr = []
+    for keyword in description_map:
+        get_image(keyword)
+        description = description_map[keyword]
+        image = overlay_text_on_image(keyword, description)
+        image_arr.append(image)
+    return image_arr
 
 
 """
@@ -28,16 +37,17 @@ Finds all the images for the given keywords and downloads them into the download
 @return:    Saves the images to the downloads folder
 """
 def get_image(keywords):
-    
+    response = google_images_download.googleimagesdownload()
     arguments = {"keywords":keywords,
     "suffix_keywords":"no watermark",
     "format":"jpg",
     "aspect_ratio":"wide",
     "usage_rights":"labeled-for-reuse-with-modifications",
-    "limit":5,
+    "limit":1,
     "size":"large",
     "print_urls":True}
     paths = response.download(arguments)
+
 
 """
 Finds the image associated with that keyword and overlays the description over the top of it
@@ -46,22 +56,27 @@ Finds the image associated with that keyword and overlays the description over t
 @return:        Saves the image to 'slides' folder
 """
 def overlay_text_on_image(keyword, description):
-    # for file in os.listdir("downloads\\" + keyword):
-    #     if file.endswith(".jpg"):
     keyword = keyword + " no watermark"
     for file in os.listdir("downloads/" + keyword):
         filename = file
-        
+    
+    font_style = "FancyHeartScript.ttf"
+    font_size = 150
+    font_placement = (50, 50)
+    font_color = (255, 150, 150)
+
+
     img = Image.open("downloads/" + keyword + "/" + filename)
 
-
+    img = img.resize((1280, 720), resample=0)
     draw = ImageDraw.Draw(img)
 
     # truetype(font.ttf, font-size)
-    font = ImageFont.truetype("FancyHeartScript.ttf", 150)
+    font = ImageFont.truetype(font_style, font_size)
 
     # text(position, text, color, font)
-    draw.text((50, 50), description, (255, 100, 100), font=font)
+    draw.text(font_placement, description, font_color, font=font)
     draw = ImageDraw.Draw(img)
-    img.save("slides/" + keyword + ".jpg")
+    img.save("slides/" + keyword + ".png")
+    return cv2.imread(filename)
 
